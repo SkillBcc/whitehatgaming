48f2710ef543e2f9b2537969ba47c603138f7c19
Igor Henderson <skillbcc@gmail.com>
2022-01-24 01:22:23 +0000

    ready to deploy

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(__dirname + '/dist/WhiteHatGamin'));
+app.use(express.static(__dirname + '/dist/WhiteHatGaming'));
 
 app.get('/*', function(req, res) {
-    res.sendFile(path.join(__dirname + '/dist/WhiteHatGamin/index.html'));
+    res.sendFile(path.join(__dirname + '/dist/WhiteHatGaming/index.html'));
 });
 
 app.listen(process.env.PORT || 8080);
